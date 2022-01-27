6a87e495a95235e6e747042037e10d37bf9fcd1d
timayyy <61433280+timayyy@users.noreply.github.com>
2022-01-27 10:03:42 +0000

    Fixed scss import typo

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-import './custom-buttom.styles.scss';
+import './custom-button.styles.scss';
 
-const CustomButton = ({ children, ...otherProps }) => (
+const CustomButton = ({ children, ...otherProps })=> (
   <button className='custom-button' {...otherProps}>
     {children}
   </button>
